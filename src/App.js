7120c08bd3b02c0c21d5c420e8704946432b1224
Bryan Alexandre <dkme01@gmail.com>
2019-08-27 17:41:35 -0300

    Saved file loaders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import Wpp from "./assets/wpp.png";
+
 function App() {
-	return <h1>Hello RocketSeat</h1>;
+	return <img width="500" src={Wpp} />;
 }
 
 export default App;
