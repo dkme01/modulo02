574f539aef560b459dd179df5c22d8198aff139b
Bryan Alexandre <dkme01@gmail.com>
2019-08-27 18:10:18 -0300

    Saved first component and some plugins

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import Wpp from "./assets/wpp.png";
+import TechList from "./components/TechList";
 
 function App() {
-	return <img width="500" src={Wpp} />;
+	return <TechList />;
 }
 
 export default App;
