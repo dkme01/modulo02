137836b8d5a2df43a013e5390f18bf2e38891181
Bryan Alexandre <dkme01@gmail.com>
2019-08-28 22:28:35 -0300

    Saved input to submit new values into state

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 
 class TechList extends Component {
+	newTech = "";
 	state = {
 		techs: ["NodeJS", "ReactJS", "React Native"]
 	};
-	render() {
-		console.log(this.state);
 
+	handleInputChange = e => {
+		this.setState({ newTech: e.target.value });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		this.setState({
+			techs: [...this.state.techs, this.state.newTech],
+			newTech: ""
+		});
+	};
+
+	render() {
 		return (
-			<ul>
-				<li>NodeJS</li>
-				<li>ReactJS</li>
-				<li>React Native</li>
-			</ul>
+			<form onSubmit={this.handleSubmit}>
+				<h1>{this.state.newTech}</h1>
+				<ul>
+					{this.state.techs.map(tech => (
+						<li key={tech}>{tech}</li>
+					))}
+				</ul>
+				<input
+					type="text"
+					onChange={this.handleInputChange}
+					value={this.state.newTech}
+				/>
+				<button type="submit">Enviar</button>
+			</form>
 		);
 	}
 }
